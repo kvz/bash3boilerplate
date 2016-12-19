abbbf7dc15fb9897dd7c96728406b8b78ef2a427
Manuel Streuhofer <manuel@streuhofer.net>
2016-12-19 20:29:52 +0100

    prepared changelog for release 2.2.0
    moved entries in changelog from `unreleased` to 2.1.0 (even
    though technically it's too late for that since that version was already
    released).
    
    added new section for 2.2.0 and added some points on what changed.
    
    bumped version to 2.2.0 in main.sh, example.sh and package.json
    
    Closes #74

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@
 #
 #  LOG_LEVEL=7 ./main.sh -f /tmp/x -d (change this for your script)
 #
-# Based on a template by BASH3 Boilerplate v2.1.0
+# Based on a template by BASH3 Boilerplate v2.2.0
 # http://bash3boilerplate.sh/#authors
 #
 # The MIT License (MIT)
